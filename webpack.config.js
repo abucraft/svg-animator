6be12bcc0128747f87204023b7f96438d4a08317
lisheng <499817366@qq.com>
2019-03-15 00:01:23 +0800

    remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,26 +24,8 @@ module.exports = {
                 test: /\.(j|t)sx?$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true,
-                        babelrc: false,
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                { targets: { browsers: 'last 2 versions' } }, // or whatever your project requires
-                            ],
-                            '@babel/preset-typescript',
-                            '@babel/preset-react',
-                        ],
-                        plugins: [
-                            // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
-                            ['@babel/plugin-proposal-decorators', { legacy: true }],
-                            ['@babel/plugin-proposal-class-properties', { loose: true }],
-                            'react-hot-loader/babel',
-                        ],
-                    },
-                },
+                    loader: 'ts-loader'
+                }
             },
             {
                 test: /\.(le|c)ss$/,
